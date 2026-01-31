28f7d878bf16203689fcbcb493e75996660cd217
burakcnaksy0 <aksoyburak808@gmail.com>
2026-01-31 14:56:50 +0300

    log page

--- web/app/page.tsx
+++ web/app/page.tsx
@@ -18,7 +18,7 @@ export default function Home() {
           </nav>
           <div className="flex items-center gap-4">
             <Link href="/login">
-              <Button variant="ghost">Sign In</Button>
+              <Button variant="ghost">Log In</Button>
             </Link>
             <Link href="/login">
               <Button>Get Not Started</Button>
